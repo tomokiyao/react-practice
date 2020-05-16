a218aef73a7b7ef4ad2dfecabfb7ccb64af15869
tomokiyao <tomoki9226@gmail.com>
2020-05-16 16:03:01 +0900

    component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
 
-function App() {
+// function App() {
+//   return (
+//     <React.Fragment>
+//       <label htmlFor="bar">bar</label>
+//       <input type="text" onChange={() => { console.log("change!!") }} />
+//     </React.Fragment>
+//   )
+// }
+
+const App = () => {
   return (
-    <React.Fragment>
-      <label htmlFor="bar">bar</label>
-      <input type="text" onChange={() => { console.log("change!!") }} />
-    </React.Fragment>
+    <div>
+      <Cat />
+      <Cat />
+      <Cat />
+    </div>
   )
+}
+
+const Cat = () => {
+  return <div>ねこ</div>
 }
 
 export default App;
